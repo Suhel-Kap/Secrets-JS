69909f01b373c2e42718d9c0b1bb5b7ecb7ba265
Suhel-Kap <suhelkapadia2@gmail.com>
2021-07-06 12:26:32 +0530

    Add md5 to hash password - Level 3

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-const dotenv = require('dotenv').config();
+const md5 = require("md5");
+
 
 const app = express();
 
@@ -18,8 +19,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = process.env.SECRET_KEY;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
 
 const User = mongoose.model("User", userSchema);
 
@@ -34,7 +33,7 @@ app
     })
     .post((req, res) => {
         const userEnteredEmail = req.body.username;
-        const userEnteredPassword = req.body.password;
+        const userEnteredPassword = md5(req.body.password);
         User.findOne({ email: userEnteredEmail }, (err, foundItem) => {
             if (err) {
                 console.log(err);
@@ -58,7 +57,7 @@ app
     .post((req, res) => {
         const newUser = new User({
             email: req.body.username,
-            password: req.body.password
+            password: md5(req.body.password)
         });
         newUser.save((err) => {
             if (err) {
